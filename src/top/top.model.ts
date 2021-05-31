995bc0ccd9e64ef8cfd371de5a8085137b568de1
Korev Dmitriy <korev.dmitriy@mail.ru>
2021-05-31 20:59:41 +0300

    add models

---
+++ src/top/top.model.ts
@@ -0,0 +1,26 @@
+enum TopCategories {
+  Courses,
+  Services,
+  Books,
+  Goods,
+}
+
+export class TopModel {
+  firstCategory: TopCategories;
+  secondCategory: string;
+  title: string;
+  category: string;
+  hh?: {
+    count: number;
+    juniorSalary: number;
+    middleSalary: number;
+    seniorSalary: number;
+  };
+  advantages: {
+    title: string;
+    description: string;
+  }[];
+  seoText: string;
+  tagsTitle: string;
+  tags: string[];
+}
